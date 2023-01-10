662d66b4419a8b263aff1d58934b54dd569c9333
Maxime PREMONT <maxime.premont@gmail.com>
2023-01-10 14:37:54 +0100

    feat(server) : Added exception handler middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'npm:express'
+import { ExceptionsHandler } from './middlewares/exception.handler.ts';
 
 const app = express();
+
+// ? Handle exceptions, needs to be the last 'use' of Express
+app.use(ExceptionsHandler)
 
 app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
