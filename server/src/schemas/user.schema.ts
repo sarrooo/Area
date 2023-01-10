27a2cbd9dddb56b21ab20b1ea92a532f95c721a2
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 20:33:03 +0100

    feat(server): add user schema validation w/ zod

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,41 @@
+import { object, string, TypeOf, z } from "zod";
+
+export const registerUserSchema = object({
+    body: object({
+        name: string({
+            required_error: "Name is required",
+        }),
+        email: string({
+            required_error: "Email is required",
+        }).email("Invalid email address"),
+        password: string({
+            required_error: "Password is required",
+        })
+            .min(8, "Password must be at least 8 characters")
+            .max(32, "Password must be less than 32 characters"),
+        passwordConfirmation: string({
+            required_error: "Password confirmation is required",
+        })
+    }).refine(data => data.password === data.passwordConfirmation, {
+        path: ["passwordConfirmation"],
+        message: "Passwords do not match"
+    })
+});
+
+export const loginUserSchema = object({
+    body: object({
+        email: string({
+            required_error: "Email is required",
+        }).email("Invalid email address"),
+        password: string({
+            required_error: "Password is required",
+        }).min(8, "Invalid email or password")
+    })
+});
+
+export type RegisterUserInput = Omit<
+    TypeOf<typeof registerUserSchema>['body'],
+    'passwordConfirmation'
+>;
+
+export type LoginUserInput = TypeOf<typeof loginUserSchema>['body'];
