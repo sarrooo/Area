2b8d4c1f5049a21861e5e0ecb5db35134df2de05
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 21:40:17 +0100

    feat(server): transform route into async route in a way to use more easily prisma & bcrypt, and hash password with 10 rounds

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,16 +1,33 @@
 import { Router } from 'express';
 import {LoginUserInput, loginUserSchema, RegisterUserInput, registerUserSchema} from "~/schemas/user.schema";
 import {validate} from "~/middlewares/validate";
+import {prisma} from "~/lib/prisma";
+import {BadRequestException} from "~/utils/exceptions";
+import bcrypt from 'bcrypt'
 
 const authRoutes = Router();
 
-authRoutes.post('/login', validate(loginUserSchema), (req, res) => {
+authRoutes.post('/login', validate(loginUserSchema), async (req, res) => {
     const { email, password }: LoginUserInput = req.body;
     res.json({ message: 'Hello World' });
 });
 
-authRoutes.post('/register', validate(registerUserSchema), (req, res) => {
+authRoutes.post('/register', validate(registerUserSchema), async (req, res) => {
     const { name, email, password }: RegisterUserInput = req.body;
+
+    const userAlreadyExist = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+    if (userAlreadyExist) {
+        throw new BadRequestException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
     res.json({ message: 'Login' });
 });
 
