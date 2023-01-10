42698eac7f8b1303c7bf42661609495afa59a80f
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 16:59:50 +0100

    feat(server): small architecture with routes folder

--- server/src/middlewares/unknowRoutes.handler.ts
+++ server/src/middlewares/unknowRoutes.handler.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from "../utils/exceptions"
+import { NotFoundException } from "~/utils/exceptions"
 
 export const UnknowRoutesHandler = () => {
     throw new NotFoundException('The requested resource does not exist')
