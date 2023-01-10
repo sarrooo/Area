ff084987f623838f552053a0904ebc09e4cfab85
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 22:55:44 +0100

    feat(server): add log info w/ auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -7,6 +7,7 @@ import {compare, hash} from 'bcrypt'
 import {StatusCodes} from "http-status-codes";
 import {sign} from "jsonwebtoken";
 import dotenv from 'dotenv';
+import Logging from "~/lib/logging";
 
 dotenv.config();
 
@@ -15,7 +16,6 @@ const authRoutes = Router();
 authRoutes.post('/login', validate(loginUserSchema), async (req, res) => {
     const { email, password }: LoginUserInput = req.body;
 
-    console.log("WSH")
     const user = await prisma.user.findUnique({
         where: {
             email: email
@@ -36,7 +36,6 @@ authRoutes.post('/login', validate(loginUserSchema), async (req, res) => {
 
     const expiredAt = new Date();
     expiredAt.setDate(expiredAt.getDate() + 7);
-
     await prisma.token.create({
         data: {
             userId: user.id,
@@ -46,6 +45,7 @@ authRoutes.post('/login', validate(loginUserSchema), async (req, res) => {
     })
 
     const token = sign({ id: user.id, name: user.email }, process.env.JWT_REFRESH_SECRET as string, { expiresIn: process.env.JWT_EXPIRES_IN_SECRET });
+    Logging.info(`User ${user.email} logged in`);
     res.status(StatusCodes.OK).json({ token });
 });
 
@@ -70,9 +70,8 @@ authRoutes.post('/register', validate(registerUserSchema), async (req, res) => {
            email: email
        }
     });
-
     const { id } = newUser;
-
+    Logging.info(`User ${email} register in`);
     return res.status(StatusCodes.CREATED).json({ id: id });
 });
 
