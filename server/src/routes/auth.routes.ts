8fedf948eb740cbee30ed646f3a07fae552840eb
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 20:54:14 +0100

    feat(server): validate data coming w/ zod Schema

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import {LoginUserInput, loginUserSchema, RegisterUserInput, registerUserSchema} from "~/schemas/user.schema";
+import {validate} from "~/middlewares/validate";
 
 const authRoutes = Router();
 
-authRoutes.get('/login', (req, res) => {
+authRoutes.post('/login', validate(loginUserSchema), (req, res) => {
+    const { email, password }: LoginUserInput = req.body;
     res.json({ message: 'Hello World' });
 });
 
-authRoutes.get('/register', (req, res) => {
+authRoutes.post('/register', validate(registerUserSchema), (req, res) => {
+    const { name, email, password }: RegisterUserInput = req.body;
     res.json({ message: 'Login' });
 });
 
--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,4 +1,4 @@
-import { object, string, TypeOf, z } from "zod";
+import { object, string, TypeOf } from "zod";
 
 export const registerUserSchema = object({
     body: object({
