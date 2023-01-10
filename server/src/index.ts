6d7b62f41696bdfc4eb7d68c120a112e19c5d4e7
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 17:34:45 +0100

    feat(server): add cookies-parser deps & add json built-in express middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,12 +5,20 @@ import {UnknowRoutesHandler} from './middlewares/unknowRoutes.handler';
 import config from 'config';
 import cors from 'cors';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const port = config.get<string>("port");
 const app = express();
 
+// Cross Origin Resource Sharing
 const frontUrl = config.get<string>("frontUrl");
 app.use(cors({ origin: frontUrl, credentials: true }));
+
+// Middleware for cookies
+app.use(cookieParser());
+
+// Built-in middleware for json
+app.use(express.json());
 
 // ? Manage unknow routes, need to be the last route
 app.all('*', UnknowRoutesHandler);
