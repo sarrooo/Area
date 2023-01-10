c99d78f60233789e23b2ad0457acff1a562e927f
Mikatech <mikael.vallenet@epitech.eu>
2023-01-10 23:59:18 +0100

    feat(server): add basic userRoute & add typeRoots in tsconfig.json

--- server/src/@types/express.d.ts
+++ server/src/@types/express.d.ts
@@ -1,4 +1,4 @@
-import { IUser } from "./user";
+import { IUser } from "~/types/user";
 
 declare global {
     namespace Express {
---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import {Request, Response, Router} from "express";
+import {verifyToken} from "~/middlewares/auth.handler";
+import authRoutes from "~/routes/auth.routes";
+
+dotenv.config();
+
+const userRouter = Router();
+
+userRouter.get('/me', verifyToken, async (req: Request, res: Response) => {
+    return res.status(200).json({ user: req.user });
+});
+
+export default userRouter;
