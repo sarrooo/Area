a53160b77fb8f00ae1f7b25f6a014a71aab1e6d7
Maxime PREMONT <maxime.premont@gmail.com>
2023-01-16 19:19:17 +0100

    feat(server) : Added api ts types

---
+++ server/src/types/api.ts
@@ -0,0 +1,55 @@
+export type Input = {
+    id: number | undefined;
+    name: string;
+    description: string | undefined;
+    regex: string | undefined;
+    mandatory: boolean;
+    type: string;
+    value: string | number | boolean | undefined;
+    linked: boolean;
+    linked_to: number | undefined;
+}
+
+export type Output = {
+    id: number | undefined;
+    name: string;
+    description: string | undefined;
+    regex: string | undefined;
+    type: string;
+    value: string | number | boolean | undefined;
+}
+
+export type Trigger = {
+    id: number | undefined;
+    name: string;
+    description: string | undefined;
+    inputs: Input[]
+    outputs: Output[]
+}
+
+export type Reaction = {
+    id: number | undefined;
+    name: string;
+    description: string | undefined;
+    inputs: Input[]
+}
+
+export type Service = {
+    id: number | undefined;
+    name: string;
+    description: string | undefined;
+    image: string | undefined;
+    triggers: Trigger[]
+    reactions: Reaction[]
+    required_subcription: boolean;
+    subscribed: boolean | undefined;
+}
+
+export type Trirea = {
+    id: number | undefined;
+    trigger: Trigger;
+    trigger_service_name: string;
+    reaction_service_name: string;
+    reaction: Reaction;
+    enabled: boolean;
+}
