c3dfa62bdb5e380ed235c4ce8d1877b84d6b10df
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2023-01-16 11:30:44 +0100

    feat(front setup): added router with landing,login,signup and notfound pages

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import App from './App'
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
     <ToastContainer />
   </React.StrictMode>
 )
