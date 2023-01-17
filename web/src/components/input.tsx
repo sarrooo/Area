9082dc48aae2c91dc9b3686bd00e055d6a8cfd90
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2023-01-17 14:21:13 +0100

    fix(components): added input component with id, and new default font poppins

--- web/src/components/input.tsx
+++ web/src/components/input.tsx
@@ -1,15 +1,31 @@
-import { InputType } from '@/types/input.type'
+export type InputProps = {
+  className?: string
+  required?: boolean
+  placeholder?: string
+  id: string
+}
 
 export const Input = ({
   className = '',
-  type,
+  required = true,
   placeholder = 'input',
-}: InputType) => {
+  id,
+}: InputProps) => {
   return (
-    <input
-      className={`text-gray400 ${className}`}
-      type={type}
-      placeholder={placeholder}
-    />
+    <div className={`p-2 ${className}`}>
+      <label
+        htmlFor={id}
+        className="block pb-2 mb-2 text-sm font-medium text-gray-900 dark:text-white text-left"
+      >
+        Last name
+        <input
+          type="text"
+          id={id}
+          required={required}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder={placeholder}
+        />
+      </label>
+    </div>
   )
 }
