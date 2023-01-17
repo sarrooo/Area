ae108eff38c8a9269fd99cd29785b7f70e432aa5
Mikatech <mikael.vallenet@epitech.eu>
2023-01-17 23:47:20 +0100

    feat(server): add twitter & github handler to router

--- server/src/routes/session.routes.ts
+++ server/src/routes/session.routes.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import {Request, Response, Router} from "express";
-import {googleOAuthHandler} from "~/controllers/auth.controller";
+import {githubOAuthHandler, googleOAuthHandler, twitterOAuthHandler} from "~/controllers/auth.controller";
 
 dotenv.config();
 
 const sessionRouter = Router();
 
 sessionRouter.get('/oauth/google', googleOAuthHandler);
+sessionRouter.get('/oauth/github', githubOAuthHandler);
+sessionRouter.get('/oauth/twitter', twitterOAuthHandler);
 
 export default sessionRouter;
