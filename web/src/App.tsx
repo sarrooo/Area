876d08bd861dd7a8891f5e8902332115b1c6610e
Nolann <62215577+Nolann71@users.noreply.github.com>
2023-01-17 18:23:48 +0100

    feat(front): regsiter ok

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Landing from '@/pages/Landing'
 import NotFound from '@/pages/NotFound'
 import Login from '@/pages/Login'
-import SignUp from '@/pages/SignUp'
+import Register from '@/pages/Register'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route index element={<Landing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
 import { BsGithub } from 'react-icons/bs'
 
-import { Input } from '@/components/input'
-import { MainButton } from '@/components/mainButton'
-import { LoginWithButton } from '@/components/loginWithButton'
+import { Input } from '@/components/Input'
+import { MainButton } from '@/components/MainButton'
+import { LoginWithButton } from '@/components/LoginWithButton'
 
 const Login = () => {
-  const test = () => {
+  const login = () => {
     console.log('test')
   }
 
@@ -18,27 +18,30 @@ const Login = () => {
       <div>
         <h1 className="text-6xl font-bold">Welcome back !</h1>
       </div>
-      <form className="p-8 bg-white shadow-xl rounded-lg w-1/3 text-center space-y-8">
-        <h1 className="text-3xl font-bold">Login</h1>
+      <form className="p-8 bg-white shadow-xl rounded-lg w-1/3 space-y-8">
+        <h1 className="text-3xl text-center font-bold">Login</h1>
         <div className="space-y-4">
-          <Input id="email" placeholder="John.doe@email.com" />
-          <Input id="password" />
+          <Input label="Email" id="email" placeholder="John.doe@email.com" />
+          <Input label="Password" id="password" />
           <div className="flex justify-around items-center">
             <p>
-              Don&apos;t have an account{' '}
-              <Link to="/signup" className="underline hover:text-blue-500">
-                create one
+              Don&apos;t have an account ?
+              <Link
+                to="/register"
+                className="text-left underline hover:text-blue-500"
+              >
+                Create one
               </Link>
             </p>
-            <MainButton callback={test} text="Login" submitter />
+            <MainButton callback={login} text="Login" submitter />
           </div>
         </div>
         <div className="h-[4px] w-full rounded-lg bg-gray-300" />
         <div className="flex flex-col justify-center items-center space-y-4">
-          <LoginWithButton text="Google" callback={test} className="w-3/4">
+          <LoginWithButton text="Google" callback={login} className="w-3/4">
             <FcGoogle />
           </LoginWithButton>
-          <LoginWithButton text="Github" callback={test} className="w-3/4">
+          <LoginWithButton text="Github" callback={login} className="w-3/4">
             <BsGithub />
           </LoginWithButton>
         </div>
