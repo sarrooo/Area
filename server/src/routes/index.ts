0c6e34d9e116c889a784d3a0bcfa99a338cbca3a
Maxime PREMONT <maxime.premont@gmail.com>
2023-01-17 16:35:02 +0100

    feat(server_route) : Added trigger input route handler

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import authRoutes from "~/routes/auth.routes";
 import userRoutes from "~/routes/user.routes";
 import triggerOutputRoutes from "~/routes/trigger_output.routes";
+import triggerInputRoutes from './trigger_input.routes';
 
 const routes = Router();
 
 routes.use('/auth', authRoutes);
 routes.use('/user', userRoutes);
 routes.use('/output/trigger', triggerOutputRoutes);
+routes.use('/input/trigger', triggerInputRoutes);
 
 export default routes;
