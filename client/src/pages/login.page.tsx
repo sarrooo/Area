a249e7cac9d0351ea22f85876ca9a8a1e2c7d486
Mikatech <mikael.vallenet@epitech.eu>
2023-01-17 23:10:44 +0100

    feat(client): add button to twitter OAUTH & github OAUTH

--- client/src/pages/login.page.tsx
+++ client/src/pages/login.page.tsx
@@ -1,6 +1,8 @@
 import { Box, Container, Typography, Link as MuiLink } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import { ReactComponent as GoogleLogo } from '../assets/google.svg';
+import { ReactComponent as GithubLogo } from '../assets/github.svg';
+import { ReactComponent as TwitterLogo } from '../assets/twitter.svg';
 import { getGoogleUrl } from '../utils/getGoogleUrl';
 
 const LoginPage = () => {
@@ -28,7 +30,7 @@ const LoginPage = () => {
                         color: 'white',
                     }}
                 >
-                    Log in with another provider:
+                    Log in with Google provider:
                 </Typography>
                 <Box
                     width='100%'
@@ -60,6 +62,96 @@ const LoginPage = () => {
                     >
                         <GoogleLogo style={{ height: '2rem' }} />
                         Google
+                    </MuiLink>
+                </Box>
+            </Box>
+            <Box width='27rem'>
+                <Typography
+                    variant='h6'
+                    component='p'
+                    sx={{
+                        my: '1.5rem',
+                        textAlign: 'center',
+                        color: 'white',
+                    }}
+                >
+                    Log in with Github provider:
+                </Typography>
+                <Box
+                    width='100%'
+                    sx={{
+                        backgroundColor: '#e5e7eb',
+                        p: { xs: '1rem', sm: '2rem' },
+                        borderRadius: 2,
+                    }}
+                >
+                    <MuiLink
+                        href={getGoogleUrl(from)}
+                        sx={{
+                            backgroundColor: '#f5f6f7',
+                            borderRadius: 1,
+                            py: '0.6rem',
+                            columnGap: '1rem',
+                            textDecoration: 'none',
+                            color: '#393e45',
+                            cursor: 'pointer',
+                            fontWeight: 500,
+                            '&:hover': {
+                                backgroundColor: '#fff',
+                                boxShadow: '0 1px 13px 0 rgb(0 0 0 / 15%)',
+                            },
+                        }}
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        <GithubLogo style={{ height: '2rem' }} />
+                        Github
+                    </MuiLink>
+                </Box>
+            </Box>
+            <Box width='27rem'>
+                <Typography
+                    variant='h6'
+                    component='p'
+                    sx={{
+                        my: '1.5rem',
+                        textAlign: 'center',
+                        color: 'white',
+                    }}
+                >
+                    Log in with Twitter provider:
+                </Typography>
+                <Box
+                    width='100%'
+                    sx={{
+                        backgroundColor: '#e5e7eb',
+                        p: { xs: '1rem', sm: '2rem' },
+                        borderRadius: 2,
+                    }}
+                >
+                    <MuiLink
+                        href={getGoogleUrl(from)}
+                        sx={{
+                            backgroundColor: '#f5f6f7',
+                            borderRadius: 1,
+                            py: '0.6rem',
+                            columnGap: '1rem',
+                            textDecoration: 'none',
+                            color: '#393e45',
+                            cursor: 'pointer',
+                            fontWeight: 500,
+                            '&:hover': {
+                                backgroundColor: '#fff',
+                                boxShadow: '0 1px 13px 0 rgb(0 0 0 / 15%)',
+                            },
+                        }}
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        <TwitterLogo style={{ height: '2rem' }} />
+                        Twitter
                     </MuiLink>
                 </Box>
             </Box>
