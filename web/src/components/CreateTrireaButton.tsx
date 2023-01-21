e853aa7255f756fac4991e14c907bde2c6e68b97
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2023-01-21 23:33:03 +0100

    feat(front) started trirea cration form modal

--- web/src/components/CreateTrireaButton.tsx
+++ web/src/components/CreateTrireaButton.tsx
@@ -1,4 +1,6 @@
 import { IoMdAdd } from 'react-icons/io'
+import { useState } from 'react'
+import { TrireaForm } from '@/components/TrireaForm'
 
 export type CreateTrireaButtonProps = {
   className?: string
@@ -6,19 +8,20 @@ export type CreateTrireaButtonProps = {
 }
 
 export const CreateTrireaButton = () => {
-  const createTrirea = () => {
-    console.log('create a trirea')
+  const [showForm, setShowForm] = useState(false)
+  const toggleForm = () => {
+    setShowForm(!showForm)
   }
 
   return (
     <button
       type="button"
-      onClick={createTrirea}
+      onClick={toggleForm}
       className="p-16 space-y-16 flex flex-col justify-center items-center border-dashed border-4 border-black rounded-lg transition ease-in-out hover:border-solid hover:bg-primary-900"
     >
       <h1 className="font-bold text-2xl">Create a trirea</h1>
-
       <IoMdAdd size={48} />
+      {showForm && <TrireaForm />}
     </button>
   )
 }
