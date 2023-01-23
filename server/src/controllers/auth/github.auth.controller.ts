fcf208177a906e6b7ea0c2308f1203c7cc2a6575
Nolann <62215577+Nolann71@users.noreply.github.com>
2023-01-23 14:32:03 +0100

    fix(oauth-backend): fix redirection to front

--- server/src/controllers/auth/github.auth.controller.ts
+++ server/src/controllers/auth/github.auth.controller.ts
@@ -1,50 +1,57 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import Logging from "~/lib/logging";
-import {BadRequestException} from "~/utils/exceptions";
-import {getGithubOauthToken, getGithubUser} from "~~/services/github-session.service";
-import {prisma} from "~/lib/prisma";
+import { BadRequestException } from "~/utils/exceptions";
+import {
+  getGithubOauthToken,
+  getGithubUser,
+} from "~~/services/github-session.service";
+import { prisma } from "~/lib/prisma";
 import config from "config";
-import {generateToken} from "~/controllers/auth/auth.controller";
+import { generateToken } from "~/controllers/auth/auth.controller";
 
-export const githubOAuthHandler = async (req: Request, res: Response, next: NextFunction) => {
-    const code = req.query.code as string;
-    const pathUrl = (req.query.state as string) || '/';
+export const githubOAuthHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const code = req.query.code as string;
+  const pathUrl = (req.query.state as string) || "/";
 
-    if (!code) {
-        Logging.warning("Github OAuth: No code provided");
-        throw new BadRequestException('No code provided');
-    }
+  if (!code) {
+    Logging.warning("Github OAuth: No code provided");
+    throw new BadRequestException("No code provided");
+  }
 
-    const { access_token } = await getGithubOauthToken({code});
-    if (!access_token) {
-        Logging.error("Github OAuth: getGithubOauthToken failed");
-        throw new BadRequestException('No access_token provided');
-    }
-    const { id, name } = await getGithubUser(access_token);
+  const { access_token } = await getGithubOauthToken({ code });
+  if (!access_token) {
+    Logging.error("Github OAuth: getGithubOauthToken failed");
+    throw new BadRequestException("No access_token provided");
+  }
+  const { id, name } = await getGithubUser(access_token);
 
-    const names: string[] = name.split(" ", 2);
-    const first_name: string = names[0];
-    const last_name: string = names[1] || "";
+  const names: string[] = name.split(" ", 2);
+  const first_name: string = names[0];
+  const last_name: string = names[1] || "";
 
-    const user = await prisma.user.upsert({
-        where: {
-            github_id: id.toString(),
-        },
-        update: {
-            first_name: first_name,
-            last_name: last_name,
-            provider: 'github',
-            github_id: id.toString(),
-        },
-        create: {
-            first_name: first_name,
-            last_name: last_name,
-            provider: 'github',
-            github_id: id.toString(),
-        }
-    })
+  const user = await prisma.user.upsert({
+    where: {
+      github_id: id.toString(),
+    },
+    update: {
+      first_name: first_name,
+      last_name: last_name,
+      provider: "github",
+      github_id: id.toString(),
+    },
+    create: {
+      first_name: first_name,
+      last_name: last_name,
+      provider: "github",
+      github_id: id.toString(),
+    },
+  });
 
-    await generateToken(user, res);
-    Logging.info(`User ${user.first_name} logged in w/ github`);
-    res.redirect(`${config.get<string>('frontUrl')}${pathUrl}`)
-}
+  await generateToken(user, res);
+  Logging.info(`User ${user.first_name} logged in w/ github`);
+  res.redirect(pathUrl);
+};
--- server/src/controllers/auth/google.auth.controller.ts
+++ server/src/controllers/auth/google.auth.controller.ts
@@ -1,57 +1,63 @@
 //TODO: Refacto the duplicata code
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import Logging from "~/lib/logging";
-import {BadRequestException} from "~/utils/exceptions";
-import {getGoogleOauthToken, getGoogleUser} from "~~/services/google-session.service";
-import {prisma} from "~/lib/prisma";
-import config from "config";
-import {generateToken} from "~/controllers/auth/auth.controller";
+import { BadRequestException } from "~/utils/exceptions";
+import {
+  getGoogleOauthToken,
+  getGoogleUser,
+} from "~~/services/google-session.service";
+import { prisma } from "~/lib/prisma";
+import { generateToken } from "~/controllers/auth/auth.controller";
 
-export const googleOAuthHandler = async (req: Request, res: Response, next: NextFunction) => {
-    const code = req.query.code as string;
-    const pathUrl = (req.query.state as string) || '/';
+export const googleOAuthHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const code = req.query.code as string;
+  const pathUrl = (req.query.state as string) || "/";
 
-    if (!code) {
-        Logging.warning("Google OAuth: No code provided");
-        throw new BadRequestException('No code provided');
-    }
+  if (!code) {
+    Logging.warning("Google OAuth: No code provided");
+    throw new BadRequestException("No code provided");
+  }
 
-    const { id_token, access_token } = await getGoogleOauthToken({code});
+  const { id_token, access_token } = await getGoogleOauthToken({ code });
 
-    const { id, given_name, family_name, verified_email } = await getGoogleUser({
-        id_token,
-        access_token,
-    });
+  const { id, given_name, family_name, verified_email } = await getGoogleUser({
+    id_token,
+    access_token,
+  });
 
-    if (!verified_email) {
-        Logging.warning("Google OAuth: Email not verified");
-        throw new BadRequestException('Google Email not verified');
-    }
+  if (!verified_email) {
+    Logging.warning("Google OAuth: Email not verified");
+    throw new BadRequestException("Google Email not verified");
+  }
 
-    const user = await prisma.user.upsert({
-        where: {
-            google_id: id,
-        },
-        update: {
-            first_name: given_name,
-            last_name: family_name,
-            provider: 'google',
-            google_id: id,
-        },
-        create: {
-            first_name: given_name,
-            last_name: family_name,
-            provider: 'google',
-            google_id: id,
-        }
-    })
+  const user = await prisma.user.upsert({
+    where: {
+      google_id: id,
+    },
+    update: {
+      first_name: given_name,
+      last_name: family_name,
+      provider: "google",
+      google_id: id,
+    },
+    create: {
+      first_name: given_name,
+      last_name: family_name,
+      provider: "google",
+      google_id: id,
+    },
+  });
 
-    if (!user) {
-        Logging.warning("Google OAuth: Failed to upsert user");
-        throw new BadRequestException("Google OAuth: Failed to upsert user");
-    }
+  if (!user) {
+    Logging.warning("Google OAuth: Failed to upsert user");
+    throw new BadRequestException("Google OAuth: Failed to upsert user");
+  }
 
-    await generateToken(user, res);
-    Logging.info(`User ${user.first_name} logged in w/ google`);
-    res.redirect(`${config.get<string>('frontUrl')}${pathUrl}`)
-}
+  await generateToken(user, res);
+  Logging.info(`User ${user.first_name} logged in w/ google`);
+  res.redirect(pathUrl);
+};
