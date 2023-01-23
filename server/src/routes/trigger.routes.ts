05bc46e8f770518685350442bcfcd7ed01a2af9d
Maxime PREMONT <maxime.premont@gmail.com>
2023-01-23 15:06:08 +0100

    feat(server_route) : Added trigger route create

--- server/src/routes/trigger.routes.ts
+++ server/src/routes/trigger.routes.ts
@@ -1,8 +1,32 @@
+import { Trigger } from '@prisma/client';
 import dotenv from 'dotenv';
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Logging from '~/lib/logging';
+import { prisma } from '~/lib/prisma';
+import { BadRequestException } from '~/utils/exceptions';
 
 dotenv.config();
 
 const triggerRoutes = Router();
+
+// Create Trigger : POST /trigger
+triggerRoutes.post('/'/*, verifyToken, */, async (req: Request, res: Response) => {
+    const {id, name, description, serviceId}: Trigger = req.body;
+    // TODO Check if user is admin
+    /*if (!is_Admin(id))
+        throw new ForbiddenRequestException("You are not allowed to create a trigger output type");*/
+    if (id !== undefined)
+        throw new BadRequestException("You cannot specify an id when creating a trigger");
+    const newTrigger: Trigger = await prisma.trigger.create({
+        data: {
+            name: name,
+            description: description,
+            serviceId: serviceId
+        }
+    });
+    Logging.info(`Trigger ${newTrigger.id} created`);
+    return res.status(StatusCodes.CREATED).json(newTrigger);
+});
 
 export default triggerRoutes; 
