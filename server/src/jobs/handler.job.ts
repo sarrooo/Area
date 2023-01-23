2dc280f8ae355ea35b5186a7e59a47022b476eb6
Mikatech <mikael.vallenet@epitech.eu>
2023-01-23 16:04:28 +0100

    feat(server): add async deps & get asynchronously trireas in cron job

--- server/src/jobs/handler.job.ts
+++ server/src/jobs/handler.job.ts
@@ -1,11 +1,28 @@
 import cron from 'node-cron';
+import {prisma} from "~/lib/prisma";
+import {each} from "async";
 
 
 //TODO: 1. Create a list of each task to run
 //TODO: 2. Create a function to run each task
 //TODO: 3. If a trigger return true, then run the reaction
-cron.schedule('* * * * *', () => {
+cron.schedule('* * * * *', async () => {
     const date = new Date();
+    const trireas = await getTrireas();
+
+    await each(trireas, async (trirea) => {
+        console.log(trirea.id);
+    });
 
     console.log(`This task is running every minute - ${date.getHours()}:${date.getMinutes()}`);
 });
+
+const getTrireas = () => {
+    return prisma.trirea.findMany(
+        {
+            where: {
+                enabled: true,
+            }
+        }
+    );
+}
