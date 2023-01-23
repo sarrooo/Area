00557264a8af82c6e938b84f19bd09b56989e2fd
Mikatech <mikael.vallenet@epitech.eu>
2023-01-23 14:45:15 +0100

    feat(server): add TODO for running triggers

--- server/src/jobs/handler.job.ts
+++ server/src/jobs/handler.job.ts
@@ -1,5 +1,9 @@
 import cron from 'node-cron';
 
+
+//TODO: 1. Create a list of each task to run
+//TODO: 2. Create a function to run each task
+//TODO: 3. If a trigger return true, then run the reaction
 cron.schedule('* * * * *', () => {
     const date = new Date();
 
