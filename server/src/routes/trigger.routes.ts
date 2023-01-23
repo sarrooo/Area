615fbf13df5c34f055ff795f63aff543fbc7b955
Maxime PREMONT <maxime.premont@gmail.com>
2023-01-23 15:30:38 +0100

    feat(server_route) : Added trigger route read

--- server/src/routes/trigger.routes.ts
+++ server/src/routes/trigger.routes.ts
@@ -7,6 +7,9 @@ import { prisma } from '~/lib/prisma';
 import { validate } from '~/middlewares/validate';
 import { createTriggerSchema } from '~/schemas/trigger.schema';
 import { BadRequestException } from '~/utils/exceptions';
+import { Trigger as ApiTrigger,
+    TriggerInputType as ApiTriggerInputType,
+    TriggerOutputType as ApiTriggerOutputType } from '~/types/api';
 
 dotenv.config();
 
@@ -29,6 +32,73 @@ triggerRoutes.post('/'/*, verifyToken, */, validate(createTriggerSchema), async 
     });
     Logging.info(`Trigger ${newTrigger.id} created`);
     return res.status(StatusCodes.CREATED).json(newTrigger);
+});
+
+async function buildTrigger(trigger: Trigger) {
+    const retTrigger: ApiTrigger = {
+        id: trigger.id,
+        name: trigger.name,
+        description: trigger.description === null ? undefined : trigger.description,
+        serviceId: trigger.serviceId,
+    };
+    // Add inputs types
+    const triggerInputTypes = await prisma.triggerInputType.findMany({
+        where: {
+            triggerId: trigger.id
+        },
+    });
+    triggerInputTypes.forEach((triggerInputType) => {
+        const addInputType: ApiTriggerInputType = {
+            id: triggerInputType.id,
+            name: triggerInputType.name,
+            type: triggerInputType.type,
+            description: triggerInputType.description === null ? undefined : triggerInputType.description,
+            regex: triggerInputType.regex === null ? undefined : triggerInputType.regex,
+            mandatory: triggerInputType.mandatory,
+            triggerId: triggerInputType.triggerId
+        };
+        if (retTrigger.inputs === undefined)
+            retTrigger.inputs = [];
+        retTrigger.inputs.push(addInputType);
+    });
+    // Add outputs types
+    const triggerOutputTypes = await prisma.triggerOutputType.findMany({
+        where: {
+            triggerId: trigger.id
+        }
+    });
+    triggerOutputTypes.forEach((triggerOutputType) => {
+        const addOutputType: ApiTriggerOutputType = {
+            id: triggerOutputType.id,
+            name: triggerOutputType.name,
+            type: triggerOutputType.type,
+            description: triggerOutputType.description === null ? undefined : triggerOutputType.description,
+            triggerId: triggerOutputType.triggerId
+        };
+        if (retTrigger.outputs === undefined)
+            retTrigger.outputs = [];
+        retTrigger.outputs.push(addOutputType);
+    });
+    return retTrigger;
+}
+
+// Read Service : GET /service/:id
+triggerRoutes.get('/:id', async (req: Request, res: Response) => {
+    const {id} = req.params;
+    try {
+        const trigger: Trigger | null = await prisma.trigger.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        if (trigger === null)
+            throw new BadRequestException("Trigger not found");
+        const retTrigger = await buildTrigger(trigger);
+        Logging.info(`Trigger ${id} read`);
+        return res.status(StatusCodes.OK).json(retTrigger);
+    } catch (_) {
+        throw new BadRequestException("Trigger not found")
+    }
 });
 
 export default triggerRoutes; 
