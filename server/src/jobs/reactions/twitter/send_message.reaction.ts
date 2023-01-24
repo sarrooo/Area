a370155174f7042d0d55ab31280e07888cc32da4
Mikatech <mikael.vallenet@epitech.eu>
2023-01-24 13:36:46 +0100

    feat(server/jobs): get reactions inputs

--- server/src/jobs/reactions/twitter/send_message.reaction.ts
+++ server/src/jobs/reactions/twitter/send_message.reaction.ts
@@ -1,6 +1,22 @@
-import {TrireaOutputs} from "~/jobs/handler.job";
+import {TrireaInputs, TrireaOutputs} from "~/jobs/handler.job";
 import * as console from "console";
+import {each} from "async";
 
 export const start = async (inputs: TrireaOutputs[]) => {
-    console.log(inputs);
+    const sendMessageInputs = await getInputs(inputs);
+    console.log(sendMessageInputs.message);
 };
+
+const getInputs = async (inputs: TrireaOutputs[]): Promise<SendMessageInputs> => {
+    let sendMessageInputs : SendMessageInputs = {message: ""};
+    await each(inputs, async (input) => {
+        if (input.triggerOutput.name === 'send_message.message' && input.value) {
+            sendMessageInputs.message = input.value;
+        }
+    });
+    return sendMessageInputs;
+}
+
+type SendMessageInputs = {
+    message: string;
+}
