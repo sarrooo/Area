493ef91c5df2ac955cd08f8f70ab9781e4da7417
Mikatech <mikael.vallenet@epitech.eu>
2023-01-24 15:59:19 +0100

    style(server/jobs): delete TODO statements

--- server/src/jobs/handler.job.ts
+++ server/src/jobs/handler.job.ts
@@ -3,21 +3,6 @@ import {prisma} from "~/lib/prisma";
 import {each} from "async";
 import * as console from "console";
 
-
-//TODO: 1. Get all trireas :white_check_mark:
-//TODO: 1.2 Loop over each trigger :white_check_mark:
-//TODO: 1.3 Get the inputs in an array forms :white_check_mark:
-//TODO: 1.4 Get the trigger function from the path :white_check_mark:
-//TODO: 1.5 Run the trigger function with the inputs :white_check_mark:
-//TODO: 1.6 If the trigger function return true, then run the reaction :white_check_mark:
-//TODO: 1.7 Get the reaction function from the path :white_check_mark:
-//TODO: 1.7.1 Get the inputs in an array forms
-//TODO: 1.8 Run the reaction function with the inputs
-//TODO: 1.9 Save the result in the database
-//TODO: 2. Create a function to run each task
-//TODO: 3. If a trigger return true, then run the reaction
-//TODO 4: Create a function to sanitize the name of the trigger and reaction
-
 cron.schedule('* * * * *', async () => {
     const date = new Date();
     const trireas = await getTrireas();
