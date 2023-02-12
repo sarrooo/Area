150cc7b02fdfab8035404280ccc06531da5249d5
Mikatech <mikael.vallenet@epitech.eu>
2023-02-12 11:18:38 +0100

    feat(jobs/on-commit): retrieve input from database

--- server/src/jobs/triggers/github/on_commit.trigger.ts
+++ server/src/jobs/triggers/github/on_commit.trigger.ts
@@ -1,6 +1,28 @@
 import {TrireaInputs} from "~/jobs/handler.job";
 import {UserService} from "@prisma/client";
+import {each} from "async";
+import Logging from "~/lib/logging";
 
 export const start = async (trireaId: number, inputs: TrireaInputs[], userServicesTrigger: UserService[], prevTriggerData: string | null): Promise<boolean> => {
+    const newTweetFromInputs = await getInputs(inputs);
+
+    if (!newTweetFromInputs.repository) {
+        Logging.warning('Trigger warning: No repository provided');
+        return false;
+    }
     return false;
 };
+
+const getInputs = async (inputs: TrireaInputs[]): Promise<OnCommitInputs> => {
+    const newTweetFromInputs : OnCommitInputs = {repository: ""};
+    await each(inputs, async (input) => {
+        if (input.triggerInputType.name === 'on_commit.repository' && input.value) {
+            newTweetFromInputs.repository = input.value;
+        }
+    });
+    return newTweetFromInputs;
+}
+
+type OnCommitInputs = {
+    repository: string;
+}
