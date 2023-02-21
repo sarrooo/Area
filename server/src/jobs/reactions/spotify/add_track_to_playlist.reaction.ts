b70da3761a91702789f9a571bb3e20e74f365d87
Mikatech <mikael.vallenet@epitech.eu>
2023-02-21 16:59:58 +0100

    feat(jobs/spotify): verification of inputs in add track to playlist reaction

--- server/src/jobs/reactions/spotify/add_track_to_playlist.reaction.ts
+++ server/src/jobs/reactions/spotify/add_track_to_playlist.reaction.ts
@@ -6,13 +6,17 @@ import Logging from "~/lib/logging";
 
 export const start = async (trireaId: number, inputs: TrireaOutputs[], userServicesReaction: UserService[]) => {
 
-    const createGistInputs = await getInputs(inputs);
-    if (!createGistInputs.description) {
-        Logging.warning('Reaction create_gist fail: No description provided');
+    const addTrackToPlaylistInputs = await getInputs(inputs);
+    if (!addTrackToPlaylistInputs.playlist_id) {
+        Logging.warning('Reaction add_track_to_playlist fail: No playlist_id provided');
         return;
     }
-    if (!createGistInputs.content) {
-        Logging.warning('Reaction create_gist fail: No content provided');
+    if (!addTrackToPlaylistInputs.artist_name) {
+        Logging.warning('Reaction add_track_to_playlist fail: No artist_name provided');
+        return;
+    }
+    if (!addTrackToPlaylistInputs.track_name) {
+        Logging.warning('Reaction add_track_to_playlist fail: No track_name provided');
         return;
     }
 
@@ -21,29 +25,8 @@ export const start = async (trireaId: number, inputs: TrireaOutputs[], userServi
         return;
     }
 
-    const githubToken = userServicesReaction[0].RefreshToken;
-    await createGist(createGistInputs.description, createGistInputs.content, createGistInputs.isPublic, githubToken);
+    const spotifyToken = userServicesReaction[0].RefreshToken;
 };
-
-const createGist = async (description: string, content: string, isPublic: boolean, githubToken: string): Promise<any> => {
-    const dataToSend = JSON.stringify({description: description, files: {"README.md": {content: content}}, public: isPublic})
-
-    try {
-        const { data } = await axios.post(
-            `https://api.github.com/gists`,
-            dataToSend,
-            {
-                headers: {
-                    "Content-type": "application/json",
-                    Authorization: `Bearer ${githubToken}`,
-                },
-            });
-        return data.data;
-    } catch (err: any) {
-        Logging.warning('Reaction create_gist fail: fail to create gist ' + err);
-        return
-    }
-}
 
 const getInputs = async (inputs: TrireaOutputs[]): Promise<AddTrackToPlaylistInputs> => {
     const addTrackToPlaylistInputs : AddTrackToPlaylistInputs = {playlist_id: '', artist_name: '', track_name: false};
