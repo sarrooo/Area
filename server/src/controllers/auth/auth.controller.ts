67b0233b436e37198b3a86dcfb650f4955d4bc97
Mikatech <mikael.vallenet@epitech.eu>
2023-02-21 15:27:06 +0100

    feat(web): delete useless token in cookie

--- server/src/controllers/auth/auth.controller.ts
+++ server/src/controllers/auth/auth.controller.ts
@@ -168,12 +168,6 @@ export const generateToken = async (
     process.env.JWT_SECRET as string,
     { expiresIn: `${config.get<number>("jwtConfig.expiresInSecret")}s` }
   );
-  res.cookie("jwtToken", token, {
-    httpOnly: true,
-    sameSite: "none",
-    maxAge: 1000 * config.get<number>("jwtConfig.expiresInSecret"),
-    secure: true,
-  });
   Logging.info(
     `Token generated for user ${config.get<number>(
       "jwtConfig.expiresInSecret"
