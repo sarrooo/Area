beb720ad8d99a307fa610ad923b7c81fa038260c
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2023-02-21 17:24:02 +0100

    feat(mobile): added native base and correct number of pages and first buttons working on dashboard

--- mobile/src/pages/Service.tsx
+++ mobile/src/pages/Service.tsx
@@ -1,11 +1,18 @@
+import {Button} from 'native-base';
 import React from 'react';
 
 import {View, Text, StyleSheet} from 'react-native';
 
-export const Service = () => {
+export const Service = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Service</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Dashboard');
+        }}>
+        Go to dashboard
+      </Button>
     </View>
   );
 };
