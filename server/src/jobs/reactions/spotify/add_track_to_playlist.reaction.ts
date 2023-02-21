70e06a00494c5673737d070544ab0ca4f0921b46
Mikatech <mikael.vallenet@epitech.eu>
2023-02-21 17:34:51 +0100

    feat(jobs/spotify): add_track_to_playlist reaction search trackID from trackName & ArtistName and add it to playlist

--- server/src/jobs/reactions/spotify/add_track_to_playlist.reaction.ts
+++ server/src/jobs/reactions/spotify/add_track_to_playlist.reaction.ts
@@ -3,6 +3,7 @@ import {each} from "async";
 import {UserService} from "@prisma/client";
 import axios from "axios";
 import Logging from "~/lib/logging";
+import qs from "qs";
 
 export const start = async (trireaId: number, inputs: TrireaOutputs[], userServicesReaction: UserService[]) => {
 
@@ -26,10 +27,55 @@ export const start = async (trireaId: number, inputs: TrireaOutputs[], userServi
     }
 
     const spotifyToken = userServicesReaction[0].RefreshToken;
+    const trackURI = await getTrackURI(addTrackToPlaylistInputs.artist_name, addTrackToPlaylistInputs.track_name, spotifyToken)
+    if (!trackURI.uri) {
+        Logging.warning('Reaction add_track_to_playlist fail: No trackURI found');
+        return;
+    }
+    await addTrackToPlaylist(addTrackToPlaylistInputs.playlist_id, trackURI.uri, spotifyToken);
 };
 
+const addTrackToPlaylist = async (playlist_id: string, trackURI: string, spotifyToken: string): Promise<any> => {
+    try {
+        const { data } = await axios.post(
+            `https://api.spotify.com/v1/playlists/${playlist_id}/tracks?uris=${trackURI}`,
+        {},
+            {
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: `Bearer ${spotifyToken}`,
+                }
+            });
+        return data;
+    } catch (err: any) {
+        Logging.warning('Reaction add_track_to_playlist fail: fail to add track to playlist' + err);
+        return
+    }
+}
+
+const getTrackURI = async (artist_name: string, track_name: string, spotifyToken: string): Promise<TrackURI> => {
+    try {
+        const options = {
+            q: 'artist:' + artist_name + ' track:' + track_name,
+            type: 'track',
+        };
+        const { data } = await axios.get<any>(
+            `https://api.spotify.com/v1/search?` + qs.stringify(options),
+            {
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: `Bearer ${spotifyToken}`,
+                }
+            });
+        return data.items[0].uri;
+    } catch (err: any) {
+        Logging.warning('Reaction like_tweet fail: fail to get user id' + err);
+        return {uri: ''}
+    }
+}
+
 const getInputs = async (inputs: TrireaOutputs[]): Promise<AddTrackToPlaylistInputs> => {
-    const addTrackToPlaylistInputs : AddTrackToPlaylistInputs = {playlist_id: '', artist_name: '', track_name: false};
+    const addTrackToPlaylistInputs : AddTrackToPlaylistInputs = {playlist_id: '', artist_name: '', track_name: ''};
     await each(inputs, async (input) => {
         if (input.reactionInputType.name === 'add_track_to_playlist.playlist_id' && input.value) {
             addTrackToPlaylistInputs.playlist_id = input.value;
@@ -38,14 +84,18 @@ const getInputs = async (inputs: TrireaOutputs[]): Promise<AddTrackToPlaylistInp
             addTrackToPlaylistInputs.artist_name = input.value;
         }
         if (input.reactionInputType.name === 'add_track_to_playlist.track_name' && input.value) {
-            addTrackToPlaylistInputs.track_name = (input.value.toLowerCase() === 'true')
+            addTrackToPlaylistInputs.track_name = input.value;
         }
     });
     return addTrackToPlaylistInputs;
 }
 
+type TrackURI = {
+    uri: string;
+}
+
 type AddTrackToPlaylistInputs = {
     playlist_id: string;
     artist_name: string;
-    track_name: boolean;
+    track_name: string;
 }
