85ab3910bf241844f57350e3d9ff9ba76546e9b3
Mikatech <mikael.vallenet@epitech.eu>
2023-02-21 16:41:43 +0100

    feat(jobs/spotify): add new trigger new playlist

---
+++ server/src/jobs/triggers/spotify/new_playlist.ts
@@ -0,0 +1,54 @@
+import {saveTriggerData, TrireaInputs} from "~/jobs/handler.job";
+import {UserService} from "@prisma/client";
+import Logging from "~/lib/logging";
+import axios from "axios";
+
+export const start = async (trireaId: number, inputs: TrireaInputs[], userServicesTrigger: UserService[], prevTriggerData: string | null): Promise<boolean> => {
+    if (userServicesTrigger.length === 0 || !userServicesTrigger[0].RefreshToken) {
+        Logging.warning('Trigger new playlist fail: No user service token provided');
+        return false;
+    }
+
+    const spotifyToken = userServicesTrigger[0].RefreshToken;
+    const playlists = await getCurrentUserPlaylist(spotifyToken)
+    if (!playlists) {
+        Logging.warning('Trigger new playlist fail: fail to fetch commit');
+        return false;
+    }
+
+    if (playlists.total === -1) {
+        return false;
+    }
+
+    if (!prevTriggerData) {
+        await saveTriggerData(trireaId, playlists.total.toString());
+        return false;
+    }
+
+    if (prevTriggerData !== playlists.total.toString()) {
+        await saveTriggerData(trireaId, playlists.total.toString());
+        return true
+    }
+
+    return false;
+};
+
+const getCurrentUserPlaylist = async (spotifyToken: string): Promise<CountPlaylist> => {
+    try {
+        const {data} = await axios.get<CountPlaylist>(
+            `https://api.spotify.com/v1/me/playlists`,
+            {
+                headers: {
+                    Authorization: `Bearer ${spotifyToken}`,
+                },
+            });
+        return data;
+    } catch (err: any) {
+        Logging.error('Trigger new playlist fail: fetch playlist' + err)
+        return {total: -1};
+    }
+}
+
+type CountPlaylist = {
+    total: number;
+}
