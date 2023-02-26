41023259727d4e1e5b626de6f3ec5b29ae0fd40d
Mikatech <mikael.vallenet@epitech.eu>
2023-02-26 11:39:00 +0100

    feat(server): adapt config to new env files

--- server/config/custom-environment-variables.ts
+++ server/config/custom-environment-variables.ts
@@ -18,16 +18,19 @@ export default {
         clientId: 'GOOGLE_OAUTH_CLIENT_ID',
         clientSecret: 'GOOGLE_OAUTH_CLIENT_SECRET',
         redirectUri: 'GOOGLE_OAUTH_REDIRECT_URL',
+        redirectConnectUri: 'GOOGLE_OAUTH_CONNECT_REDIRECT_URL',
     },
     githubConfig: {
         clientId: 'GITHUB_OAUTH_CLIENT_ID',
         clientSecret: 'GITHUB_OAUTH_CLIENT_SECRET',
         redirectUri: 'GITHUB_OAUTH_REDIRECT_URL',
+        redirectConnectUri: 'GITHUB_OAUTH_CONNECT_REDIRECT_URL',
     },
     twitterConfig: {
         clientId: 'TWITTER_OAUTH_CLIENT_ID',
         clientSecret: 'TWITTER_OAUTH_CLIENT_SECRET',
         redirectUri: 'TWITTER_OAUTH_REDIRECT_URL',
+        redirectConnectUri: 'TWITTER_OAUTH_CONNECT_REDIRECT_URL',
     },
     spotifyConfig: {
         clientId: 'SPOTIFY_OAUTH_CLIENT_ID',
