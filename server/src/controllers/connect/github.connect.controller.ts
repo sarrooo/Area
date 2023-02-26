f388715ed4c277032444c57e250a66660f0cf22e
Mikatech <mikael.vallenet@epitech.eu>
2023-02-26 11:56:53 +0100

    feat(server): add github connect oauth handler controller

---
+++ server/src/controllers/connect/github.connect.controller.ts
@@ -0,0 +1,72 @@
+import { NextFunction, Request, Response } from "express";
+import Logging from "~/lib/logging";
+import {BadRequestException, ForbiddenRequestException, UnauthorizedRequestException} from "~/utils/exceptions";
+import {prisma} from "~/lib/prisma";
+import {verify} from "jsonwebtoken";
+import {getGithubConnectOauthToken} from "~~/services/github-session.service";
+
+export const githubConnectHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const refreshToken = req.cookies["refreshToken"];
+    if (!refreshToken) {
+        throw new ForbiddenRequestException("No refresh token");
+    }
+
+    let payload: any;
+    try {
+        payload = verify(refreshToken, process.env.JWT_REFRESH_SECRET as string);
+    } catch (error) {
+        Logging.error(error);
+        throw new UnauthorizedRequestException("Invalid refresh token");
+    }
+    if (!payload) {
+        throw new ForbiddenRequestException("Invalid refresh token");
+    }
+
+    const code = req.query.code as string;
+
+    if (!code) {
+        Logging.error("Github Connect OAuth: No code provided");
+        throw new BadRequestException("No code provided");
+    }
+
+    const { access_token } = await getGithubConnectOauthToken({ code });
+    if (!access_token) {
+        Logging.error("Github Connect OAuth: getGithubConnectOauthToken failed");
+        throw new BadRequestException("No access_token provided");
+    }
+
+    const github = await prisma.service.findUnique({
+        where: {
+            name: "github"
+        }
+    });
+    if (!github) {
+        Logging.error("Github Connect OAuth: No github service found");
+        throw new BadRequestException("No github service found");
+    }
+
+    await prisma.userService.upsert({
+        where: {
+            userId_serviceId: {
+                userId: payload.id,
+                serviceId: github.id
+            }
+        },
+        update: {
+            RefreshToken: access_token
+        },
+        create: {
+            userId: payload.id,
+            serviceId: github.id,
+            RefreshToken: access_token
+        }
+    });
+
+    res.redirect(
+        `${process.env.CORS_FRONT_URL}/oauth_callback`
+    );
+};
