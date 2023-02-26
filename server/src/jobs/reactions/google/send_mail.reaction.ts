333d243bf12957f55f9953f5b8c8eebd32c97d3e
Mikatech <mikael.vallenet@epitech.eu>
2023-02-26 22:20:02 +0100

    feat(server): retrieving input of send_mail google reaction

---
+++ server/src/jobs/reactions/google/send_mail.reaction.ts
@@ -0,0 +1,101 @@
+import {TrireaOutputs} from "~/jobs/handler.job";
+import {each} from "async";
+import {UserService} from "@prisma/client";
+import axios from "axios";
+import Logging from "~/lib/logging";
+
+export const start = async (trireaId: number, inputs: TrireaOutputs[], userServicesReaction: UserService[]) => {
+
+    const sendMailInputs = await getInputs(inputs);
+    if (!sendMailInputs.to) {
+        Logging.warning('Reaction send mail fail: No to provided');
+        return;
+    }
+    if (!sendMailInputs.subject) {
+        Logging.warning('Reaction send mail fail: No subject provided');
+        return;
+    }
+    if (!sendMailInputs.content) {
+        Logging.warning('Reaction send mail fail: No content provided');
+        return;
+    }
+
+    if (userServicesReaction.length === 0 || !userServicesReaction[0].RefreshToken) {
+        Logging.warning('Reaction like_tweet fail: No user service token provided');
+        return;
+    }
+
+    const twitterToken = userServicesReaction[0].RefreshToken;
+    const userID = await getUserID(twitterToken)
+    if (!userID) {
+        Logging.warning('Reaction like_tweet fail: No userID found');
+    }
+    await likeTweet(sendMailInputs.id, userID, twitterToken);
+};
+
+const likeTweet = async (tweetID: string, userID: string, twitterToken: string): Promise<any> => {
+    const dataToSend = JSON.stringify({tweet_id: tweetID})
+
+    try {
+        const { data } = await axios.post(
+            `https://api.twitter.com/2/users/${userID}/likes`,
+            dataToSend,
+            {
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: `Bearer ${twitterToken}`,
+                },
+            });
+        return data.data;
+    } catch (err: any) {
+        Logging.warning('Reaction like_tweet fail: fail to like tweet to target' + err);
+        return
+    }
+}
+
+const getUserID = async (twitterToken: string): Promise<string> => {
+    try {
+        const { data } = await axios.get<twitterUser>(
+            `https://api.twitter.com/2/users/me`,
+            {
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: `Bearer ${twitterToken}`,
+                }
+            });
+        return data.data.id;
+    } catch (err: any) {
+        Logging.warning('Reaction like_tweet fail: fail to get user id' + err);
+        return ""
+    }
+}
+
+const getInputs = async (inputs: TrireaOutputs[]): Promise<SendMailsInputs> => {
+    const sendMailsInputs : SendMailsInputs = {to: '', subject: '', content: ''};
+    await each(inputs, async (input) => {
+        if (input.reactionInputType.name === 'send_mail.to' && input.value) {
+            sendMailsInputs.to = input.value;
+        }
+        if (input.reactionInputType.name === 'send_mail.subject' && input.value) {
+            sendMailsInputs.subject = input.value;
+        }
+        if (input.reactionInputType.name === 'send_mail.content' && input.value) {
+            sendMailsInputs.content = input.value;
+        }
+    });
+    return sendMailsInputs;
+}
+
+type SendMailsInputs = {
+    to: string;
+    subject: string;
+    content: string;
+}
+
+type twitterUser = {
+    data: {
+        id: string;
+        name: string;
+        username: string;
+    }
+}
