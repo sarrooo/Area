3c973a2d91dd830c666d2e7be775693283612c2a
Mikatech <mikael.vallenet@epitech.eu>
2023-02-27 14:38:29 +0100

    feat(web): add facebook to oauth map

--- web/src/utils/oauth/index.tsx
+++ web/src/utils/oauth/index.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react'
-import { BsGithub, BsGoogle, BsTwitter, BsSpotify } from 'react-icons/bs'
-import { getOauthGithubUrl} from './github'
+import { BsGithub, BsGoogle, BsTwitter, BsSpotify, BsFacebook} from 'react-icons/bs'
+import { getOauthGithubUrl } from './github'
 import { getOauthConnectGoogleUrl } from './google'
 import { getOauthConnectTwitterUrl } from './twitter'
 import { getOauthConnectSpotifyUrl } from '@/utils/oauth/spotify'
+import { getOauthConnectFacebookUrl } from '@/utils/oauth/facebook'
 
 export type MappingOauth = {
   name: string
@@ -31,5 +32,10 @@ export const mappingOauth: MappingOauth[] = [
     name: 'spotify',
     url: getOauthConnectSpotifyUrl(),
     icon: <BsSpotify />,
+  },
+  {
+    name: 'facebook',
+    url: getOauthConnectFacebookUrl(),
+    icon: <BsFacebook />,
   },
 ]
