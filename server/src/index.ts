d79fcfa87e44bea854921d4a2ec470048f740175
Maxime PREMONT <maxime.premont@gmail.com>
2023-03-04 15:29:26 +0100

    feat(deploy) : Https deploy setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,13 @@ const port = config.get<number>('port');
 
 const app = express();
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
 // Cross Origin Resource Sharing
 app.use(cors({ origin: process.env.CORS_FRONT_URL, credentials: true }));
 
@@ -38,9 +45,10 @@ app.all('*', UnknowRoutesHandler);
 // ? Handle exceptions, needs to be the last 'use' of Express
 app.use(ExceptionsHandler);
 
-//TODO: Fix and use env var
-app.listen(port, () => {
-    Logging.success(`Server listening on port ${port}`);
-});
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(8443);
 
 module.exports = app;
