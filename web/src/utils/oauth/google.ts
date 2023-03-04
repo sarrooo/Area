99440aeb8c72c55433b42287c4ee5106d0b05d96
Nolann <62215577+Nolann71@users.noreply.github.com>
2023-03-04 17:01:58 +0100

    fix: fix scope

--- web/src/utils/oauth/google.ts
+++ web/src/utils/oauth/google.ts
@@ -10,7 +10,7 @@ export const getOauthGoogleUrl = () => {
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email',
-      'https//mail.google.com',
+      'https://mail.google.com',
     ].join(' '),
     state: window.location.href,
   }
