4daf94782cdaccccfe65f48c7f563fcbeb6267c5
Nolann <62215577+Nolann71@users.noreply.github.com>
2023-03-07 15:31:35 +0100

    hot: debug

--- server/src/utils/req.ts
+++ server/src/utils/req.ts
@@ -3,6 +3,6 @@ import { Request } from "express";
 export const getRedirectUri = (req: Request) => {
   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
 
-  if (!url.includes('?platform')) return url.split('&')[0];
+  if (url.includes('?platform')) return url.split('&')[0];
   return url.split('?')[0];
 }
